5f9d69daa716345ab038c78b9fbe3924c7b7c299
Zaptoss <zaptosgg@gmail.com>
2025-09-26 13:08:33 +0300

    Return create_group method

--- rust/src/api/group_context.rs
+++ rust/src/api/group_context.rs
@@ -273,27 +273,6 @@ pub struct BGroupContext {
 
 impl BGroupContext {
     #[flutter_rust_bridge::frb(sync)]
-    pub fn new(
-        identity_secret_key: Vec<u8>,
-        user: BUser,
-        group_info: BGroupInfo,
-    ) -> Result<(Self, Vec<u8>)> {
-        let identity_secret_key = ScalarField::deserialize_compressed(&identity_secret_key[..])
-            .map_err(|e| anyhow!("failed to deserialize: {}", e.to_string()))?;
-
-        let mut group_info = group_info.group_info;
-        group_info.members_mut().insert_user(user.user);
-
-        let (group_context, frame) = GroupContext::new(identity_secret_key, group_info)
-            .map_err(|e| anyhow!("failed to deserialize: {}", e.to_string()))?;
-        let frame = frame
-            .encode_to_vec()
-            .map_err(|e| anyhow!("failed to deserialize: {}", e.to_string()))?;
-
-        Ok((Self { group_context }, frame))
-    }
-
-    #[flutter_rust_bridge::frb(sync)]
     pub fn from_parts(
         identity_secret_key: Vec<u8>,
         leaf_secret: Vec<u8>,
@@ -570,4 +549,25 @@ pub fn public_key_from_secret_key(secret_key: Vec<u8>) -> Result<Vec<u8>> {
         .map_err(|e| anyhow!("failed to serialize: {}", e.to_string()))?;
 
     Ok(public_key_bytes)
+}
+
+#[flutter_rust_bridge::frb(sync)]
+pub fn create_group(
+    identity_secret_key: Vec<u8>,
+    user: BUser,
+    group_info: BGroupInfo,
+) -> Result<(BGroupContext, Vec<u8>)> {
+    let identity_secret_key = ScalarField::deserialize_compressed(&identity_secret_key[..])
+        .map_err(|e| anyhow!("failed to deserialize: {}", e.to_string()))?;
+
+    let mut group_info = group_info.group_info;
+    group_info.members_mut().insert_user(user.user);
+
+    let (group_context, frame) = GroupContext::new(identity_secret_key, group_info)
+        .map_err(|e| anyhow!("failed to deserialize: {}", e.to_string()))?;
+    let frame = frame
+        .encode_to_vec()
+        .map_err(|e| anyhow!("failed to deserialize: {}", e.to_string()))?;
+
+    Ok((BGroupContext { group_context }, frame))
 }
